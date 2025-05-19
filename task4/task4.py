cd80152de44d00f8063c36beecaf96033980dbee
Светлана Гимазова <gimazova.s@yandex.ru>
2025-05-19 13:37:06 +0400

    fix task4

--- task4/task4.py
+++ task4/task4.py
@@ -1,22 +1,27 @@
-def list_create(path):
+import sys
+
+
+def read_list(path):
     with open(path, 'r') as file:
-        nums = []
-        for num in file:
-            n = int(num)
-            nums.append(n)
-    return nums
+        return [int(line.strip()) for line in file]
 
 
-def get_average(num_list):
-    ave = int(sum(num_list) / len(num_list))
-    counts = 0
+def get_median(num_list):
+    num_list.sort()
+    median = num_list[len(num_list)//2]
+    sum = 0
     for num in num_list:
-        counts += abs(num-ave)
-    print(counts)
+        sum += abs(num - median)
+    return sum
+
 
 
 def main():
-    get_average(list_create('numbers.txt'))
+    if len(sys.argv) != 2:
+        print('Данных недостаточно!')
+        sys.exit(1)
+
+    print(get_median(read_list(sys.argv[1])))
 
 
 main()
